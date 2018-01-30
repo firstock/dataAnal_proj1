fd017c1a21fcf4bba062a15361a1294a93917c08
firstock <optimist20@naver.com>
2018-01-30 18:23:02 +0900

    학원 cleaning 끝냄

--- clean_1_price.R
+++ clean_1_price.R
@@ -32,10 +32,7 @@ names(price1)
 
 ## 특정 칼럼빼고 다시 저장
 outName <- "item"
-# outName <- "tot"
-# outName <- "payaim"
-# outName <- "jobless"
-# outName <- "youthemploy"
+# "item", "tot", "payaim", "jobless", "youthemploy"
 
 
 ## 일반
@@ -44,10 +41,63 @@ write.csv( price1[,-grep("계|(구분)",colnames(price1))], paste0("data/price_"
 ## transpose 했던거
 # write.csv( price1, paste0("data/price_",outName,"_clean1.csv"), row.names=FALSE)
 
-##엑셀 작업. <<< transpose 한 것 해당
+###엑셀 작업. <<< transpose 한 것 해당
 # transpose한 건 첫 줄 빼주고, 칼럼은 몇개 안되면 노가다. 많으면 함더 불러서 코딩
 
-price2 <- read.csv(paste0("data/price_",outName,"_clean1.csv"), header=T, sep=",")
-head(price2,2)
-names(price2)
 
+
+outName <- "youthemploy"
+# "item" # ok
+# "tot" # ok
+# "payaim" #ok
+# "jobless" #ok
+# "youthemploy"
+### merge 위한 밑작업
+price2 <- read.csv(paste0("data/price_",outName,"_clean1.csv"), header=T, sep=",")
+
+
+## 첫 칼럼 Date로
+(nl <- names(price2))
+(names(price2) <- c("Date",nl[-1]))
+head(price2$Date,40)
+
+## 같은 형식으로 바꾸기
+# 년월은 as.Date()불가. 날짜 붙이는건 조작
+
+# # item
+# reg_find <- "(\\d{4}).0?(\\d)"
+# reg_out <- "\\1-0\\2"
+# price2$Date <- gsub(reg_find,reg_out,price2$Date)
+# reg_find <- "\\-0(\\d{2})"
+# reg_out <- "-\\1"
+# price2$Date <- gsub(reg_find,reg_out,price2$Date)
+
+#tot, jobless, youthemploy
+reg_find <- "(\\d{4})(\\d{2})"
+reg_out <- "\\1-\\2"
+price2$Date <- gsub(reg_find,reg_out,price2$Date)
+
+# #payaim
+# reg_find <- "(\\d{4})\\.(\\d{2})"
+# reg_out <- "\\1-\\2"
+# price2$Date <- gsub(reg_find,reg_out,price2$Date)
+# reg_find <- "(\\.1)"
+# reg_out <- "-10"
+# price2$Date <- gsub(reg_find,reg_out,price2$Date)
+
+#
+
+
+
+
+head(price2$Date,40)
+
+
+
+### cleaning 최종
+write.csv(price2, paste0("data/price_",outName,"_clean2.csv"), row.names=FALSE)
+# 날짜 형식이면 알아서 날짜가 붙음 @엑셀. 데려올땐 괜찮
+
+
+# price3 <- read.csv(paste0("data/price_",outName,"_clean2.csv"), header=T, sep=",")
+# head(price3,3)
--- merge_clean2_price.R
+++ merge_clean2_price.R
@@ -1,13 +1,42 @@
+#cleaning 이 완전히 끝나고 merge해야!
+
 # setwd("E:/github/dataAnal_proj1")
 setwd("E:/github/dataAnal_proj1/data")
-file_list= list.files(pattern="_clean1\\.csv$")
 
-dataset= data.frame()
-for(file in file_list){
-  # print(file)
-  temp =  read.csv(file, header=T, sep=",")
-  head(temp,2) #왜 안읽힘??
-}
-# for(outName,i in zip(outNames,1:length(outNames))){
-#   price[i] <- read.csv(paste0("data/price_",outName,"_clean1.csv"), header=T, sep=",")
+# 아 모르겠다ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ아 ㅆ 40분하고 안됨
+# ## read files
+# 
+# dataset= data.frame()
+# for i,file in zip(1:length(file_list),file_list)){
+#   dataset[i] <- read.csv(file, header=T, sep=",")
+#   #warning 50개
+rbind대신 merge 넣을 수 있게 cleaning해둬야!
+#   dataset= rbind(dataset, temp) #행 길이달라서 안됨
 # }
+
+
+(file_list= list.files(pattern="*_clean1\\.csv$"))
+
+file <- file_list[1]
+price1 <- read.csv(file, header=T, sep=",")
+file <- file_list[2]
+price2 <- read.csv(file, header=T, sep=",")
+file <- file_list[3]
+price3 <- read.csv(file, header=T, sep=",")
+file <- file_list[4]
+price4 <- read.csv(file, header=T, sep=",")
+file <- file_list[5]
+price5 <- read.csv(file, header=T, sep=",")
+
+length(price1[,1])
+length(price2[,1])
+length(price3[,1])
+length(price4[,1])
+length(price5[,1])
+
+head(price1,1)
+head(price2,1)
+head(price3,1)
+head(price4,1)
+head(price5,1)
+
